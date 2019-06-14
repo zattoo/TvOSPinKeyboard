4b381bd61cec3111e86bff3d90577aeb31b0c5f6
Matteo Gobbi <matteo_gobbi@me.com>
2019-06-13 21:45:58 -0700

    Made the class available for @objc and better delegate method
    Added ViewController param into the delegate's method

--- Sources/TvOSPinKeyboardViewDelegate.swift
+++ Sources/TvOSPinKeyboardViewDelegate.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
+@objc
 public protocol TvOSPinKeyboardViewDelegate: class {
     
-    func pinKeyboardDidEndEditing(pinCode: String)
+    @objc func pinKeyboardDidEndEditing(pinKeyboardVC: TvOSPinKeyboardViewController, pinCode: String)
 }
